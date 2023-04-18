cf68ec6375f4674770430f86fd60e8d15b28557a
unseenPrime <dimaturokt23@gmail.com>
2023-04-18 12:15:09 +0300

    Зробив бургер меню.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-"use strict"
+// Клас для додавання/видалення класу active
+function toggleMenu() {
+  document.querySelector('.menu__list').classList.toggle('active');
+  document.querySelector('.burger').classList.toggle('active');
+}
 
+// Навішуємо обробник подій на клік по бургер-меню
+document.querySelector('.burger').addEventListener('click', toggleMenu);
