083642239f309a98eb6a8d236aeeb7c459f08a81
unseenPrime <dimaturokt23@gmail.com>
2023-04-18 17:58:26 +0300

    Зверстав слайдер, без адаптиву.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-// Клас для додавання/видалення класу active
+// A class for adding/removing the active class
 function toggleMenu() {
   document.querySelector('.menu__list').classList.toggle('active');
   document.querySelector('.burger').classList.toggle('active');
 }
 
-// Навішуємо обробник подій на клік по бургер-меню
+// Attach an event handler to a click on the burger menu
 document.querySelector('.burger').addEventListener('click', toggleMenu);
+//
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    cssEase: 'linear',
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
